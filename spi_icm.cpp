cb0c479a01186631a0995ed2843cf1dd832826b3
Maria Sanchez <maria.sanchez@dfki.de>
2017-03-17 16:50:58 +0100

    g

--- spi_icm.cpp
+++ spi_icm.cpp
@@ -32,7 +32,7 @@ main()
     spi->mode(mraa::SPI_MODE3);
 	//spi->lsbmode(0);
 
-    uint8_t txBuf[1] = {WHO_AM_I_REG };
+    uint8_t txBuf[2] = {WHO_AM_I_REG, 0xFF };
     uint8_t rxBuf[2] = {0 , 0};
     uint8_t* recv;
 
