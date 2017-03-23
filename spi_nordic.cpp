b43c515a779872de72092b37847d1736e935a6da
Maria Sanchez <maria.sanchez@dfki.de>
2017-03-23 16:33:27 +0100

    jngjng

--- spi_nordic.cpp
+++ spi_nordic.cpp
@@ -60,14 +60,14 @@ int main(){
 		gpio->write(0);
 		if (spi->transfer(NULL, rxBuf,14) == mraa::SUCCESS) {
 	    	gpio->write(1);
-  		    time0 = (rxBuf[3]<<24) | (rxBuf[2]<<16) | (rxBuf[1]<<8) |rxBuf[0] ;
-  		    time = (rxBuf[13]<<24) | (rxBuf[12]<<16) | (rxBuf[11]<<8) |rxBuf[10] ;
+  		    time = (rxBuf[3]<<24) | (rxBuf[2]<<16) | (rxBuf[1]<<8) |rxBuf[0] ;
+//  		time0 = (rxBuf[13]<<24) | (rxBuf[12]<<16) | (rxBuf[11]<<8) |rxBuf[10] ;
         	currentDiff = time-prevTime;
 		    if(time !=0){  
 			     j++;
-   		  	     fprintf(fileWrite,"\nRaw0 0x%.2x%.2x%.2x%.2x ",rxBuf[3],rxBuf[2],rxBuf[1],rxBuf[0]);
-   		       	 fprintf(fileWrite," Raw1 0x%.2x%.2x%.2x%.2x ",rxBuf[13],rxBuf[12],rxBuf[11],rxBuf[10]);
-		       	 fprintf(fileWrite," Time: %d; Time0: %d; DifTime: %d	", time, time0, currentDiff);
+   		  	     fprintf(fileWrite,"\nRaw 0x%.2x%.2x%.2x%.2x ",rxBuf[3],rxBuf[2],rxBuf[1],rxBuf[0]);
+   		         fprintf(fileWrite,"CheckBytes 0x%.2x%.2x%.2x%.2x ",rxBuf[13],rxBuf[12],rxBuf[11],rxBuf[10]);
+		       	 fprintf(fileWrite," Time: %d; DifTime: %d	", time,  currentDiff);
 		    }else{
 		       	i++;
 		    }           
